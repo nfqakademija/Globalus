539703a1489ceac610189fa09db238e516b19c5d
Arnoldas Milkus <arnoldas8@gmail.com>
2016-10-31 21:52:03 +0200

    added registracion confirm via email

--- src/AppBundle/Controller/PostController.php
+++ src/AppBundle/Controller/PostController.php
@@ -9,6 +9,7 @@
 namespace AppBundle\Controller;
 
 
+use AppBundle\Mailer\Mailer;
 use FOS\UserBundle\FOSUserBundle;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -18,10 +19,14 @@ use AppBundle\Entity\User;
 use AppBundle\Form\PostType;
 use AppBundle\Form\LoginType;
 use AppBundle\Form\UserType;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use FOS\UserBundle\Form\Type\RegistrationFormType;
 class PostController extends Controller
 {
-
+    /**
+     * @var EventDispatcherInterface
+     */
+    private $eventDispatcher;
     /**
      * @Route("/list")
      */
@@ -103,10 +108,13 @@ class PostController extends Controller
             $user->setEmail($user->getEmail());
             $user->setPlainPassword($user->getPassword())*/
             //TODO send email
-            $user->setEnabled(1);
+            $user->setEnabled(false);
+            $random_hash = md5(uniqid(rand(), true));
+            $user->setConfirmationToken($random_hash);
             //$user->addRole('ROLE_ADMIN');
             $user->addRole('ROLE_USER');
-
+            $this->sendAction($user,$random_hash);
+            //$this->eventDispatcher->dispatch("app.create_user");
             //$user->setCreatedAt(new \DateTime());
             $em = $this->getDoctrine()->getManager();
             $em->persist($user);
@@ -131,6 +139,9 @@ class PostController extends Controller
         $posts = $exampleService->getPostsFromDb();// getPostsById($id);
         $repo = $this->getDoctrine()->getManager()->getRepository('AppBundle:User');
         $post = $repo->find($id);
+        /*$mailer = Mailer();
+
+        $mailer->sendConfirmationEmailMessage($post);*/
         return $this->render('AppBundle:Post:successReg.html.twig',
             [
 
@@ -138,5 +149,48 @@ class PostController extends Controller
 
             ]
         );
+    }
+
+
+    public function sendAction(User $user, $confirmationToken)
+    {
+
+        $message = \Swift_Message::newInstance()
+            ->setSubject('Pabaikite registraciją')
+            ->setFrom('nfqglobalus@gmail.com')
+            ->setTo($user->getEmail())
+            ->setBody(
+                $this->renderView(
+                // app/Resources/views/Emails/registration.html.twig
+                    'AppBundle:Email:registration.html.twig',
+                    array('name' => $user->getUsername(),
+                        'token' => $confirmationToken)
+                ),
+                'text/html'
+            )
+
+        ;
+        /*$this->container->get('swiftmailer.email_sender.listener');
+        $this->get('swiftmailer.mailer.abstract');*/
+        $this->get('swiftmailer.mailer.default')->send($message);
+
+        return $this->render('@App/Home/index.html.twig');
+    }
+
+    /**
+     * @Route("/confirmUser/{confirmationToken}")
+     */
+    public function confirmUser($confirmationToken){
+        $exampleService = $this->get('app.example');
+        $user = $exampleService->enableUser($confirmationToken);
+        if($user==null){
+            echo "klaida";
+        }else {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            //$em->refresh($user);
+            $em->flush();
+            return $this->render('@App/Post/createdUser.html.twig');
+        }
     }
 }
--- src/AppBundle/Service/ExampleService.php
+++ src/AppBundle/Service/ExampleService.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Service;
 
 use Doctrine\ORM\EntityManager;
+use Symfony\Component\Config\Definition\Exception\Exception;
 
 class ExampleService
 {
@@ -41,5 +42,26 @@ class ExampleService
         $posts = $repository->find($id);
 
         return $posts;
+    }
+    public function enableUser($confirmationToken){
+        $repository = $this->em->getRepository('AppBundle:User');
+        $q = $this->em->createQuery("select u from AppBundle\Entity\User u ");
+        $users = $q->getResult();
+        /*$repository =$this->em->createQueryBuilder();
+        $user = $repository->select('*')->from('user')->where("confirmation_token ='.$confirmationToken.'");*/
+        //$user = $repository->findOneBy(array('confirmation_token' => $confirmationToken));
+        foreach ($users as $user){
+            if($user->getConfirmationToken()===$confirmationToken)
+            {
+                $user->setEnabled(true);
+                $user->setConfirmationToken(null);
+                //$q = $this->em->createQuery("select u from AppBundle\Entity\User u ");
+                return $user;
+            }
+        }
+
+        /**/
+        //throw new Exception();
+        return null;
     }
 }
