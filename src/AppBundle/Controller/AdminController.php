c034622fcca85f9039a99bef5023bea974be15e2
Arnoldas Milkus <arnoldas8@gmail.com>
2016-11-12 23:35:46 +0200

    added user delete action

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -7,6 +7,7 @@
  */
 
 namespace AppBundle\Controller;
+use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use AppBundle\Entity\User;
@@ -23,7 +24,7 @@ class AdminController extends Controller
         return $this->render('AppBundle:Admin:index.html.twig', []);
     }
     /**
-     * @Route("/user", name="userAction")
+     * @Route("/user", name="users")
      */
     public function userAction()
     {
@@ -32,5 +33,29 @@ class AdminController extends Controller
         return $this->render('AppBundle:Admin:user.html.twig', [
             'users' => $users
         ]);
+    }
+    /**
+     * @Route("/user/{id}", name="userAction")
+     */
+    public function userActionList($id)
+    {
+        $userService = $this->get('app.user');
+        $user = $userService->getUserById($id);
+        return $this->render('AppBundle:Admin:userAction.html.twig', [
+            'user' => $user
+        ]);
+    }
+    /**
+     * @Route("/user/delete/{id}", name="userDelete")
+     */
+    public function userDelete($id)
+    {
+        $userService = $this->get('app.user');
+
+        $user = $userService->getUserById($id);
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($user);
+        $em->flush();
+        return $this->render('AppBundle:Admin:index.html.twig', []);
     }
 }
